f7d81419c6b38dde6890d70f5f7b5651039741a5
Danny Page <daniel.w.page@gmail.com>
2015-03-29 23:20:31 -0400

    Set sparkline helper to use pixels, not percentage, for height. Height reconfigurable as well.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,13 @@
 module ApplicationHelper
   def load_sparkline(player)
+    max_spark_height = 30
     load_values = player.last_7_training_loads.map(&:value)
     max_value = load_values.max
     lines = load_values.map { |value|
-      percent = (100 * value.to_f / max_value).round
-      %Q[<div style="background-color: black; display: inline-block; height: #{percent}%; width: 4px; margin-right: 1px;"></div>]
+      pixels = (( value.to_f / max_value) * max_spark_height ).round
+      %Q[<div style="background-color: black; display: inline-block; height: #{pixels}px; width: 4px; margin-right: 1px;"></div>]
     }.join('')
 
-    %Q[<div style="height: 30px;">#{lines}</div>].html_safe
+    %Q[<div style="height: #{max_spark_height}px;">#{lines}</div>].html_safe
   end
 end
