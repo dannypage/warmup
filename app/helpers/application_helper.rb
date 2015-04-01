ff3271a12b81b49a5bf2c11901ad62c2bd060484
Danny Page <daniel.w.page@gmail.com>
2015-04-01 07:15:43 -0400

    Fixing the look of the sparkline, next to check the values.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,9 +5,10 @@ module ApplicationHelper
     max_value = load_values.max
     lines = load_values.map { |value|
       pixels = (( value.to_f / max_value) * max_spark_height ).round
+      pixels = pixels > 0 ? pixels : 1  #Ensure that there's no blanks.
       %Q[<div style="background-color: black; display: inline-block; height: #{pixels}px; width: 4px; margin-right: 1px;"></div>]
     }.join('')
 
-    %Q[<div style="height: #{max_spark_height}px;">#{lines}</div>].html_safe
+    %Q[<div title= '#{load_values}'>#{lines}</div>].html_safe
   end
 end
